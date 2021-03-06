b2b4843db514394ea5be2ede92ec3cd80da4f405
pavle <markovic.pavle@outlook.com>
2021-03-06 20:45:39 +0100

    Modified data_preprocessing script to work with full dataset. Added .gitignore file.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,54 +1,67 @@
 from collections import Counter
 import argparse
+import os
+
 
 def arg_parser():
     """ takes user input """
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--input_file", default='data/sample.conll',
-                        help='use this option to provide a file (in CONLL format)')
-    parser.add_argument("--output_dir", default='data/',
-                        help="use this option to provide the output directory path")
+    parser.add_argument("--data_dir", default='data/ontonetes-4.0',
+                        help='use this option to provide a path to data dir (default=data/ontonetes-4.0).')
+    parser.add_argument("--out_dir", default='data/',
+                        help="use this option to provide the output dir (default=data/)")
 
     args = parser.parse_args()
     return args
 
 
-def create_tsv_and_fetch_info(input_file, out_dir):
+def create_tsv_and_fetch_info(args):
     """
-    generates sample.tsv file from sample.conll and collects information from the data while iterating over each sentence
+    generates tsv file from conll files and collects information from the data while iterating over each sentence
     """
+
     sequence_len_list = []  #contains length of every sequence
     num_words_per_tag = Counter()
+    data_path = os.path.join(os.getcwd(), args.data_dir)
+    output_file = os.path.join(os.getcwd(), args.out_dir, 'ontonetes-4.0.tsv')
 
-    with open(out_dir+'sample.tsv', 'w+') as of:
-        with open(input_file, 'r') as f:
-            sents = f.readlines()
-            count = 0
+    with open(output_file, 'w+') as of:
 
-            for i in range(len(sents)):
+        for file in os.listdir(data_path):
+            if not file.endswith(".gold_conll"): # sanity check for conll files
+                continue
 
-                if sents[i].startswith('#'):  # ignore lines starting with #
-                    continue
+            with open(os.path.join(data_path, file), 'r') as f:
+                count = 0
 
-                if sents[i] == '\n':  # marks the end of sequence
-                    sequence_len_list.append(count)
-                    count = 0  # reset the count
-                    of.write('*\n')
-                    continue
+                for sent in f:
+                    if sent.startswith('#'):  # ignore lines starting with #
+                        continue
 
-                tokens = sents[i].split()
-                num_words_per_tag[tokens[4]] += 1
-                of.write(f'{tokens[2]}\t{tokens[3]}\t{tokens[4]}\n')
-                count += 1  # update count of the sequence
+                    if sent == '\n':  # marks the end of sequence
+                        sequence_len_list.append(count)
+                        count = 0  # reset the count
+                        of.write('*\n')
+                        continue
+                    
+                    # 2th token is word index
+                    # 3rd token is word
+                    # 4th token is POS tag
+                    tokens = sent.split()
+                    num_words_per_tag[tokens[4]] += 1
+                    of.write(f'{tokens[2]}\t{tokens[3]}\t{tokens[4]}\n')
+                    count += 1  # update count of the sequence
 
     return num_words_per_tag, sorted(sequence_len_list)
 
 
-def write_info_to_file(out_dir, num_words_per_tag, sequence_len_list):
+def write_info_to_file(args, num_words_per_tag, sequence_len_list):
     total_words = sum(num_words_per_tag.values())
+    output_file = os.path.join(os.getcwd(), args.out_dir, 'ontonetes-4.0.info')
 
-    with open(out_dir+'sample.info','w+') as f:
+    with open(output_file, 'w+') as f:
         f.write(f'Maximum sequence length: {sequence_len_list[-1]}\n')
         f.write(f'Minimum sequence length: {sequence_len_list[0]}\n')
         f.write(f'Mean sequence length: {sum(sequence_len_list) / len(sequence_len_list)}\n')
@@ -59,10 +72,8 @@ def write_info_to_file(out_dir, num_words_per_tag, sequence_len_list):
             f.write(f'{tag}\t{(count / total_words) * 100}%\n')
 
 
-def main():
-    args = arg_parser()
-    num_words_per_tag, sequence_len_list = create_tsv_and_fetch_info(input_file=args.input_file, out_dir=args.output_dir)
-    write_info_to_file(out_dir=args.output_dir, num_words_per_tag=num_words_per_tag, sequence_len_list=sequence_len_list)
-
 if __name__ == '__main__':
-    main()
+    args = arg_parser()
+    
+    num_words_per_tag, sequence_len_list = create_tsv_and_fetch_info(args)
+    write_info_to_file(args, num_words_per_tag, sequence_len_list)
