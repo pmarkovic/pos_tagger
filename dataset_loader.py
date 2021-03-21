f5e7b90d67da0360d365d5c4093719a08422c18f
rrichajalota <rrichajalota@gmail.com>
2021-03-21 10:08:57 +0100

    integration w hack-script, batched embedding generation

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,6 +1,9 @@
 import argparse
 import logging
-from pprint import pprint
+from collections import defaultdict
+from copy import deepcopy
+from pathlib import Path
+import pickle as pkl
 
 import torch
 from transformers import BertTokenizer, AutoTokenizer, AutoModel
@@ -83,25 +86,80 @@ def arg_parser():
     return args
 
 
+def create_input_batches(dataset, batch_size):
+    batched_wordLists = []
+    input_batches = []
+    pos_tags = []
+    for sent in dataset['train']:
+        batched_wordLists.append(sent['word_list'])
+        pos_tags.append(sent['tag_list'])
+        if len(batched_wordLists) % batch_size == 0:
+            input_batches.append((batched_wordLists, pos_tags))
+            batched_wordLists = []
+            pos_tags = []
+    input_batches.append((batched_wordLists, pos_tags))
+    return input_batches
+
+
 if __name__ == '__main__':
     args = arg_parser()
     dataset = datasets.load_dataset('dataset_loader.py', data_files={'train':args.train, 'test': args.test})
     tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased') # With AutoTokenizer, huggingface uses faster rust implementation of the tokenizer
     model = AutoModel.from_pretrained('bert-base-multilingual-cased')
 
-    batch_wordlists = []
-    for sent in dataset['train']:
-        batch_wordlists.append(sent['word_list'])
+    input_batches = create_input_batches(dataset, batch_size=50)
+    print(len(input_batches))
+    Path("./temp_data").mkdir(parents=True, exist_ok=True)
 
-    # offset_map returns start and end positions of bert tokens (0,0) for special tokens
-    # https://stackoverflow.com/questions/66666525/how-to-map-token-indices-from-the-squad-data-to-tokens-from-bert-tokenizer
-    encoded_input_with_offset_mapping = tokenizer(batch_wordlists, is_split_into_words=True, padding=True, return_tensors='pt', return_offsets_mapping=True)
+    # generate embeddings
+    for batched_wordLists, batched_ptags in input_batches:
+        # offset_map returns (char_start, char_end) for each token. (0,0) for special tokens
+        # https://stackoverflow.com/questions/66666525/how-to-map-token-indices-from-the-squad-data-to-tokens-from-bert-tokenizer
+        encoded_input_with_offset_mapping = tokenizer(batched_wordLists, is_split_into_words=True, padding=True, return_tensors='pt', return_offsets_mapping=True)
+        encoded_input = deepcopy(encoded_input_with_offset_mapping)
+        encoded_input.pop('offset_mapping')
+        # logging.info(f"encoded_input.keys() : {encoded_input.keys()}, encoded_input_with_offset_mapping.keys() {encoded_input_with_offset_mapping.keys()}")
 
-    encoded_input = encoded_input_with_offset_mapping
-    encoded_input.pop('offset_mapping')
-    logging.info(encoded_input.keys(), encoded_input_with_offset_mapping.keys())
+        batched_word2token_position = [] # for every wordlist in the batch, stores [(word1, pos_tag1, token_start_pos, token_end_pos),..]
 
-    embeddings = model(**encoded_input)[0].squeeze() # MemoryError on CPU - TODO: test on collab
+        for i, wordlist in enumerate(batched_wordLists):
+            #print(wordlist)
+            word2token_position = []
+            k = 1
+            for j, word in enumerate(wordlist):
+                tup = encoded_input_with_offset_mapping['offset_mapping'][i][k]
+                start_pos = k
+                tup_len = tup[1] - tup[0]
+                if len(word) == tup_len:
+                    end_pos = k
+                else: # iterate over the following tuples
+                    while len(word) != tup_len:
+                        k += 1
+                        tup = encoded_input_with_offset_mapping['offset_mapping'][i][k]
+                        tup_len += tup[1] - tup[0]
+                    end_pos = k
+                word2token_position.append((word, batched_ptags[i][j], start_pos, end_pos + 1)) # (word, pos_tag, token_start_pos, token_end_pos)
+                k += 1
+            #print(word2token_position)
+            batched_word2token_position.append(word2token_position)
+
+        embeddings = model(**encoded_input)[0].squeeze()
+        #print(embeddings)
+        # Create dict where keys are POS tags, and values are lists of embeddings
+        # of all words that we encountered in dataset
+        # Note for one POS tag we can have many embeddings for the same words
+        # That can be good if these embeddings are different due to different contexts
+        # But we can restrict that to include only one embeddings per word per POS tag
+        tokens_embed = defaultdict(list)
+        for ind, word2token_position in enumerate(batched_word2token_position):
+            for tup in word2token_position:
+                pos_tag = tup[1]
+                embed = torch.mean(embeddings[ind][tup[2]:tup[3]], dim=0)
+                tokens_embed[pos_tag].append(embed)
+                #print(pos_tag, embed)
+                with open('temp_data/ptag_embed_batch'+str(ind)+'.pkl', 'wb') as f:
+                    pkl.dump(tokens_embed, f)
+
 
 
 
